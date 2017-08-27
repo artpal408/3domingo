6d53b0cf395e04db5aa6caf1e0da8167db193ae9
Lab2Ies <artpal2014@gmail.com>
2017-08-27 13:37:51 -0600

    mi primer commit.

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
+<form action="consulta.php">
+
+    <input name="nombre" type="text">
+    <br>
+    <input name="edad" type="text">
+    <br>
+    <input name="ciudad" type="text">
+    <br>
+
+    <input value="enviar" type="submit">
+
+
+</form>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Lab2Ies
- * Date: 27/08/2017
- * Time: 13:24
- */
+
+
+echo 'hola';
+
+
+
+
